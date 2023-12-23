bb018862c905a74914d2d6cfe9676915fc260537
Soni kumari <Sonikumari78707@gmail.com>
2023-12-23 14:24:26 +0530

    invite

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
-const endDate = "23 December 2023 05:20:00 PM"
+const endDate = "4 December 2024 00:00:00 AM"
 
 document.getElementById("end-date").innerText = endDate;
 
 const inputs = document.querySelectorAll("input")
-    // const clock = () => {
 
-    // }
+// const clock = () => {
+// }
 
-// 1st jan 1997 to sofar(abtak) millisecond covert endDate
+// 1st jan 1997 to sofar(abtak)--- endDate covert millisecond(new Date)
 function clock() {
     const end = new Date(endDate)
     const now = new Date()
+
+	// console.log(end);
+	// console.log(now);
+
+	// const diff = (end - now);
+	// console.log(diff);
+
     const diff = (end - now) / 1000;
 
+	// -me nhi aayega
     if (diff < 0) return;
 
     // convert into days;
     inputs[0].value = Math.floor(diff / 3600 / 24);
+	// day
     inputs[1].value = Math.floor(diff / 3600) % 24;
+	// hrs
     inputs[2].value = Math.floor(diff / 60) % 60;
+	// mins
     inputs[3].value = Math.floor(diff) % 60;
+	// sec
 }
 
 // initial call
@@ -29,11 +41,12 @@ clock()
  *  1 day = 24 hrs
  *  1 hr = 60 mins
  *  60 min = 3600 sec
- */
+*/
 
- setInterval(
+// 1sec(1000) in call clock()
+setInterval(
     () => {
         clock()
     },
     1000
- )
+)
